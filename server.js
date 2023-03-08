6a7144a530d1e59085862e63b430214bdbb6e3f2
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-03-08 13:18:38 +0100

    ..

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3000
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.listen(PORT, () => console.log(`Server is rumming... on prot ${PORT}`))
