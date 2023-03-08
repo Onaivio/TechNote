c0cc818097ccb1139c2ef1a65aed672628ff0f2d
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-03-08 18:00:15 +0100

    ..

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {logger} = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger)
+
+app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 app.all('*', (req, res) => {
     res.status(404)
