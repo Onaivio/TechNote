ccb1965df2ca3a0ee40a682623138f7a1dbafbd1
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-03-11 23:45:32 +0100

    ..

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const {logger} = require('./middleware/logger')
+const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require("./config/corsOptions")
 const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
-const {logEvents} = require('./middleware/logger')
+const { logEvents } = require('./middleware/logger')
 const PORT = process.env.PORT || 3500
 
 console.log(process.env.NODE_ENV)
@@ -29,12 +29,12 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 app.all('*', (req, res) => {
     res.status(404)
-    if(req.accepts('html')){
+    if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if(req.accepts('json')){
-        res.json({error: 'Not found'})
-        res.json({message: '404 Not found'})
-    }else {
+    } else if (req.accepts('json')) {
+        res.json({ error: 'Not found' })
+        res.json({ message: '404 Not found' })
+    } else {
         res.type('txt').send('404 Not found')
     }
 })
