0afde7c4fded07e095279ced881c3cc962cc731d
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-03-11 23:55:15 +0100

    ..

--- models/Note.js
+++ models/Note.js
@@ -27,11 +27,11 @@ const noteSchema = new mongoose.Schema(
 );
 
 noteSchema.plugin(
-    AutoIncrement,{
-        inc_field: 'ticket',
-        id: 'ticketNums',
-        start_seq: 500
-    }
+  AutoIncrement, {
+  inc_field: 'ticket',
+  id: 'ticketNums',
+  start_seq: 500
+}
 )
 
 module.exports = mongoose.model("Note", noteSchema);
