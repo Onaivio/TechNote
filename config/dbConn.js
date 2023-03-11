3f1e0e12f2b60ba7afdac1b3e4e0c3192de49c2b
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-03-11 23:55:32 +0100

    ..

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    try{
+    try {
         await mongoose.connect(process.env.DATABASE_URI)
-    } catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
